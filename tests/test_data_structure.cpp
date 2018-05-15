53172d39b80b1fafd115e7b0caf4a7711ca5ee37
Duc-Minh Phan <alephvn@gmail.com>
2018-05-15 08:19:28 +0200

    Write the first test, update the buildsystem generator

---
+++ tests/test_data_structure.cpp
@@ -0,0 +1,23 @@
+#include "catch.hpp"
+#include "data_structure.hpp"
+
+bool test_stop_times(const Timetable& timetable) {
+    for (const auto& route: timetable.routes()) {
+        for (int j = 0; j < route.stops.size() - 1; ++j) {
+            for (int i = 0; i < route.trips.size(); ++i) {
+                if (route.stop_times[i][j].arr > route.stop_times[i][j + 1].arr) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+TEST_CASE("Check if the stop times are in order", "[stop_times]") {
+    Timetable timetable {"Paris"};
+    timetable.summary();
+
+    CHECK(test_stop_times(timetable));
+}
