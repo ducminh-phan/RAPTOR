16df14e8c99099a033170081a838f9a7aada7562
Duc-Minh Phan <alephvn@gmail.com>
2018-06-18 23:05:37 +0200

    Add the command line arguments helper

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,37 @@
 #include "experiments.hpp"
 
 
-int main() {
-    Timetable timetable {"Paris"};
+void usage_exit(char* argv[]) {
+    auto paragraph = [](std::string s, size_t width = 80) -> std::string {
+        std::string acc;
+        while (!s.empty()) {
+            size_t pos = s.size();
+            if (pos > width) pos = s.rfind(' ', width);
+            std::string line = s.substr(0, pos);
+            acc += line + "\n";
+            s = s.substr(pos);
+
+            // Remove the space starting the line
+            if (s[0] == ' ') s = s.substr(1);
+        }
+        return acc;
+    };
+
+    std::cerr << "Usage: " << argv[0] << " name\n\n"
+              << paragraph("Run the RAPTOR algorithm with the generated queries and log "
+                           "the running time and arrival times of each query.\n")
+              << "Positional arguments:\n"
+              << " name\tThe name of the dataset to be used in the algorithm";
+    exit(1);
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        usage_exit(argv);
+    }
+
+    Timetable timetable {argv[1]};
     timetable.summary();
 
     Experiment exp {&timetable};
