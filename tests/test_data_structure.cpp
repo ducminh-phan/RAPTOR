2eeb8d70e29591af0e01f455b4b179226c351cf0
Duc-Minh Phan <alephvn@gmail.com>
2018-06-21 17:17:29 +0200

    Write the test case to check if all trips of each route have the same stop pattern

--- tests/test_data_structure.cpp
+++ tests/test_data_structure.cpp
@@ -1,10 +1,12 @@
 #include "catch.hpp"
 #include "data_structure.hpp"
 
-bool test_stop_times(const Timetable& timetable) {
+
+// The columns in each stop_times table are ordered
+bool test_stop_times_ordered(const Timetable& timetable) {
     for (const auto& route: timetable.routes()) {
-        for (int j = 0; j < route.stops.size() - 1; ++j) {
-            for (int i = 0; i < route.trips.size(); ++i) {
+        for (size_t j = 0; j < route.stops.size() - 1; ++j) {
+            for (size_t i = 0; i < route.trips.size(); ++i) {
                 if (route.stop_times[i][j].arr > route.stop_times[i][j + 1].arr) {
                     return false;
                 }
@@ -15,9 +17,34 @@ bool test_stop_times(const Timetable& timetable) {
     return true;
 }
 
-TEST_CASE("Check if the stop times are in order", "[stop_times]") {
+
+// The trips of a route have the same stop pattern
+bool test_unique_pattern(const Timetable& timetable) {
+    for (const auto& route: timetable.routes()) {
+        auto stops = route.stops;
+
+        for (const auto& row: route.stop_times) {
+            // The stop pattern of this row (trip)
+            std::vector<node_id_t> row_stops;
+            for (const auto& st: row) {
+                row_stops.push_back(st.stop_id);
+            }
+
+            if (stops != row_stops) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+
+TEST_CASE("Test the sanity of the dataset and parser", "") {
     Timetable timetable {"Paris", "R"};
     timetable.summary();
 
-    CHECK(test_stop_times(timetable));
+    REQUIRE(test_stop_times_ordered(timetable));
+
+    REQUIRE(test_unique_pattern(timetable));
 }
